9be286916555c259efcceb871c525cc936b0a627
anaksha17 <k224293@nu.edu.pk>
2025-02-14 15:20:06 +0500

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,1 @@
-from app import app
-from models import db
-
-# Create the tables in the database
-with app.app_context():
-    db.create_all()  # This will create the tables based on the models defined in models.py
-    print("Tables created successfully!")
+ 
